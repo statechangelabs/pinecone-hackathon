54c42225438f1c377c9744ec41bdb6f42e43f6d4
Ray Deck <ray@raydeck.com>
2023-06-25 22:40:46 -0400

    feat: AR idea - fix the text

--- app/src/Repo.tsx
+++ app/src/Repo.tsx
@@ -48,17 +48,17 @@ const Repo: FC = () => {
               //   defaultValue={""}
             />
           </div>
-          //submit
+
           <div className="flex-shrink-0">
             <button
               onClick={submitForm}
               type="submit"
               className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              Post
+              Ask for help
             </button>
           </div>
-          //see the answer
+
           {replyId && <Answer replyId={replyId} />}
         </div>
       )}
